92e383b22ac05bb5e69a4ef6757037fa43123c9b
vcokltfre <vcokltfre@gmail.com>
2021-01-27 04:41:26 +0000

    feat: add options to supress only stdout or stderr

--- supress/__init__.py
+++ supress/__init__.py
@@ -1,6 +1,4 @@
 import sys
-import contextlib
-from io import BytesIO
 
 
 class supressed:
@@ -23,4 +21,38 @@ class supressed:
     def flush(self):
         pass
 
-__all__ = (supressed)
+class supressed_out:
+    def __init__(self):
+        self.old_out = sys.stdout
+
+    def __enter__(self):
+        sys.stdout = self
+        return self
+
+    def __exit__(self, *args):
+        sys.stdout = self.old_out
+
+    def write(self, _):
+        pass
+
+    def flush(self):
+        pass
+
+class supressed_err:
+    def __init__(self):
+        self.old_err = sys.stderr
+
+    def __enter__(self):
+        sys.stderr = self
+        return self
+
+    def __exit__(self, *args):
+        sys.stderr = self.old_err
+
+    def write(self, _):
+        pass
+
+    def flush(self):
+        pass
+
+__all__ = (supressed, supressed_out, supressed_err)
